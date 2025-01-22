21b66a96f7d85c39941ee3838327bb26c7ef73a8
piyushprashantkopte <piyushkopte23@gmail.com>
2025-01-22 16:34:56 +0530

    commented in Header.jsx

--- src/components/other/Header.jsx
+++ src/components/other/Header.jsx
@@ -18,7 +18,7 @@ const Header = (props) => {
 
   return (
     <div className='flex items-end justify-between'>
-      <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'> User 👋</span></h1>
+      <h1 className='text-2xl font-medium'>Hello <br /><span className='text-3xl font-semibold'> User 👋</span></h1>   {/*changing the name according */}
       <button onClick={logoutUser} className='bg-red-500 text-white px-5 py-2 rounded-sm text-lg font-medium'>Log Out</button>
     </div>
   )
